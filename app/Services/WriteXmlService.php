e5463fdb868bbbc8661430322296e65bf2dc5300
unknown <dgloria.web@gmail.com>
2024-06-15 19:31:11 +0100

    some results

--- app/Services/WriteXmlService.php
+++ app/Services/WriteXmlService.php
@@ -3,6 +3,7 @@
 namespace app\Services;
 
 use Illuminate\Support\Facades\Storage;
+use PHPUnit\Event\Code\Test;
 use Spatie\ArrayToXml\ArrayToXml;
 
 class WriteXmlService
@@ -10,26 +11,115 @@ class WriteXmlService
 
     public function parseCsv()
     {
+        /* array should look like this:
 
-        $row = 1;
+        $testarray =
+            [
+                "product" => [
+                    [
+                        '_attributes' =>
+                        [
+                            'product_id' => ''
+                        ],
+                        "display-name" =>
+                        [
+                            '_attributes' => ['_xml:lang' => ''],
+                            "__text" => ""
+                        ],
+                        "brand" => "",
+                        "variations" => [
+                            "variants" => [
+                                "variant"   => [
+                                    [
+                                        "_product-id" => "",
+                                        "_default" => ""
+                                    ],
+                                    [
+                                        "_product-id" => ""
+                                    ]
+                                ]
+                            ],
+                        ],
+                        "custom-attributes" =>
+                        [
+                            "custom-attribute" =>
+                            [
+                                [
+                                    "_attribute-id" => "",
+                                    "__text" => "",
+                                ],
+                                [
+                                    "_attribute-id" => "",
+                                    "__text" => "",
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ];
+            */
         $productsarray = [];
+        $existingProductKey = 0;
         if (($handle = fopen("products.csv", "r")) !== FALSE) {
             while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-                $num = count($data);
-                $productsarray[] = $data;
-                $row++;
-                for ($c = 0; $c < $num; $c++) {
-                    //format data to match the required xml format
+                //if product id already exists, then modify build 
+                $prodId = $data[0];
 
+                $brand = $data[1];
+                $displayName = $data[2];
+                $variantProductId = $data[3];
+                $variantCustomAttrColor = $data[4];
+                $variantCustomAttrSize = $data[5];
+                $variantDefault = $data[6];
+                //variants should separately appear under the product section ??? how?
+                //if product already exists, then add the variants only, but can't add [0] as
+                $existingProductKey = array_search($prodId, array_column($productsarray, "product_id")); // this doesn't find it. 
+                if ($existingProductKey === false) {
+
+                    $productsarray["product"]["_attributes"]["product_id"] = $prodId;
+                    $productsarray["product"]["display-name"]["_value"] = $displayName;
+                    $productsarray["product"]["display-name"]["_attributes"]["_xml:lang"] = "x-default";
+                    $productsarray["product"]["brand"] = $brand;
+                    // if has more variants, then append
+                    $productsarray["product"]["variations"]["variants"]["variant"]["_attributes"]["_product-id"] = $variantProductId;
+                    if ($variantDefault == "Y") {
+                        $productsarray["product"]["variations"]["variants"]["variant"]["_attributes"]["default"] = "true";
+                    }
+                } else {    
+                    $productsarray[$existingProductKey]["product"]["variations"]["variants"]["variant"]["_attributes"]["_product-id"] = $variantProductId;
+                    if ($variantDefault == "Y") {
+                        $productsarray[$existingProductKey]["product"]["variations"]["variants"]["variant"]["_attributes"]["default"] = "true";
+                    }
                 }
+
+
+
+                $productsarray["product"]["custom-attributes"]["custom-attribute"]["_attribute-id"] = "color";
+                $productsarray["product"]["custom-attributes"]["custom-attribute"]["__text"] = $variantCustomAttrColor;
+
+                // if has custom attributes then append
+                $productsarray["product"]["custom-attributes"]["custom-attribute"]["_attribute-id"] = "size";
+                $productsarray["product"]["custom-attributes"]["custom-attribute"]["__text"] = $variantCustomAttrSize;
+
+                // should have done an object instead...
+
+
             }
             fclose($handle);
         }
+        // dd($productsarray);
         return $productsarray;
     }
     public function arrayToXml($productsarray)
     {
-        $result = arrayToXml::convert($productsarray);
+        $result = ArrayToXml::convert($productsarray, [
+            'rootElementName' => 'catalog',
+            '_attributes' => [
+                'xmlns' => 'http://www.demandware.com/xml/impex/catalog/2006-10-31',
+                'catalog_id' => 'TestCatalog'
+            ],
+        ], true, 'UTF-8');
+
         Storage::put('public/products.xml', $result);
     }
 }
