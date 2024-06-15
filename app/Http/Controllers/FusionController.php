63d8f96c175e8c59ae6f9803381cebecba6ef70f
unknown <dgloria.web@gmail.com>
2024-06-15 16:13:24 +0100

    added some code and the xml test output

--- app/Http/Controllers/FusionController.php
+++ app/Http/Controllers/FusionController.php
@@ -17,18 +17,30 @@ class FusionController extends BaseController
     {
         // return view('welcome');
         // read csv into array
+        $productsarray = $this->parseCsv();
+        
+        return dd($productsarray);
+
+    }
+
+    public function parseCsv()
+    {
 
         $row = 1;
+        $productsarray = [];
         if (($handle = fopen("products.csv", "r")) !== FALSE) {
             while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
                 $num = count($data);
-                echo "<p> $num fields in line $row: <br /></p>\n";
+                $productsarray[] = $data;
+                // echo "<p> $num fields in line $row: <br /></p>\n";
                 $row++;
                 for ($c = 0; $c < $num; $c++) {
-                    echo $data[$c] . "<br />\n";
+                    // echo $data[$c] . "<br />\n";
                 }
             }
             fclose($handle);
         }
+        return $productsarray;
     }
+
 }
