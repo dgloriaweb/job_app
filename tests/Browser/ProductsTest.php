b155d932defa1ad4d419d48fa8aae86d385210da
unknown <dgloria.web@gmail.com>
2024-06-18 18:36:46 +0100

    getting closer

--- tests/Browser/ProductsTest.php
+++ tests/Browser/ProductsTest.php
@@ -18,6 +18,7 @@ class ProductsTest extends DuskTestCase
 
     public function test_products_xml(): void
     {
+        $this->buildDocument();
         $this->assertXmlFileEqualsXmlFile(
             "public/test1.xml",
             "public/files/prodsfromtest.xml"
@@ -32,10 +33,6 @@ class ProductsTest extends DuskTestCase
         $xml->startDocument();
         $xml->startCatalog('TestCatalog');
         $xml->writeEntity($this->buildProductElement());
-        // $xml->writeEntity($this->buildMinimalProductElement());
-        // $xml->writeEntity($this->buildSetElement());
-        // $xml->writeEntity($this->buildBundleElement());
-        // $xml->writeEntity($this->buildVariationElement());
         $xml->endCatalog();
         $xml->endDocument();
 
@@ -44,21 +41,22 @@ class ProductsTest extends DuskTestCase
     protected function buildProductElement(): Product
     {
         $element = $this->buildBaseElement('Product');
-        $element->setClassificationCategory('CAT123', 'TestCatalog');
-        $element->setSitemap(1.0);
-        $element->setImages(['product-123.png']);
-        $element->setSharedVariationAttributes(['AT001', 'AT002']);
+        // $element->setClassificationCategory('CAT123', 'TestCatalog');
+        // $element->setSitemap(1.0);
+        // // $element->setEncoding();
+        // $element->setImages(['product-123.png']);
+        // $element->setSharedVariationAttributes(['AT001', 'AT002']);
         $element->setVariants([
             'SKU0000001' => false,
             'SKU0000002' => false,
             'SKU0000003' => true,
         ]);
 
-        $element->addVariationGroups([
-            'PRODUCT123-Red',
-            'PRODUCT123-Yellow',
-            'PRODUCT123-Green',
-        ]);
+        // $element->addVariationGroups([
+        //     'PRODUCT123-Red',
+        //     'PRODUCT123-Yellow',
+        //     'PRODUCT123-Green',
+        // ]);
 
         return $element;
     }
@@ -69,25 +67,25 @@ class ProductsTest extends DuskTestCase
 
         $element = new Product(mb_strtoupper($type) . '123');
         $element->setDisplayName($type . ' number 123');
-        $element->setLongDescription('<b>' . $type . '</b> The description for an <i>example</i> ' . mb_strtolower($type) . '! • Bullet' . $invalidChar1 . 'Point');
-        $element->setUpc('50000000000' . $number);
-        $element->setQuantities(); // include, but use defaults
-        $element->setSearchRank(1);
+        // $element->setLongDescription('<b>' . $type . '</b> The description for an <i>example</i> ' . mb_strtolower($type) . '! • Bullet' . $invalidChar1 . 'Point');
+        // $element->setUpc('50000000000' . $number);
+        // $element->setQuantities(); // include, but use defaults
+        // $element->setSearchRank(1);
         $element->setBrand('SampleBrand™');
-        $element->setOnlineFlag(true);
+        // $element->setOnlineFlag(true);
         $element->setSearchableFlags(null, false, null);
 
-        $element->setOnlineFromTo(
-            new DateTimeImmutable('2015-01-23 01:23:45'),
-            new DateTimeImmutable('2025-01-23 01:23:45')
-        );
+        // $element->setOnlineFromTo(
+        //     new DateTimeImmutable('2015-01-23 01:23:45'),
+        //     new DateTimeImmutable('2025-01-23 01:23:45')
+        // );
 
-        $element->setPageAttributes(
-            'Amazing ' . $type,
-            'Buy our ' . $type . ' today!',
-            $type . ', test, example',
-            'http://example.com/' . mb_strtolower($type) . '/123'
-        );
+        // $element->setPageAttributes(
+        //     'Amazing ' . $type,
+        //     'Buy our ' . $type . ' today!',
+        //     $type . ', test, example',
+        //     'http://example.com/' . mb_strtolower($type) . '/123'
+        // );
 
         $element->addCustomAttributes([
             'type'         => 'Examples',
@@ -98,7 +96,7 @@ class ProductsTest extends DuskTestCase
             'boolFalse'    => false,
         ]);
 
-        $element->setImages([mb_strtolower($type) . '-123.png']);
+        // $element->setImages([mb_strtolower($type) . '-123.png']);
 
         return $element;
     }
