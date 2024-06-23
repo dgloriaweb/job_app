4294ddc1029dd0d79bc07e68a9643ada199a7a28
unknown <dgloria.web@gmail.com>
2024-06-23 18:59:56 +0100

    added the variants list to the main product

--- tests/Browser/ProductsTest.php
+++ tests/Browser/ProductsTest.php
@@ -15,6 +15,7 @@ class ProductsTest extends DuskTestCase
     /**
      * A Dusk test example.
      */
+    private $variantArray = []; // this is holding the variants to feed to the setter
 
     public function test_products_xml(): void
     {
@@ -41,7 +42,7 @@ class ProductsTest extends DuskTestCase
 
                 // write the array. 
                 // first element is the main product with id, display name, brand
-                if ($savedProductId == "") {
+                if ($savedProductId == "" || $savedProductId != $data[0]) {
                     $productsArray[$data[0]] = [
                         "product_id" => $data[0],
                         "brand" => $data[1],
@@ -49,13 +50,10 @@ class ProductsTest extends DuskTestCase
                         "variations" => [
                             "variants" => [
                                 "variant" =>
-                                [
-                                    "_attributes"
-                                    => [
-                                        "_product-id" => $data[3],
-                                        "default" => $defaultVariant
-                                    ]
-                                ]
+                                [[
+                                    "_product-id" => $data[3],
+                                    "default" => $defaultVariant
+                                ]]
                             ]
                         ]
                     ];
@@ -63,12 +61,9 @@ class ProductsTest extends DuskTestCase
                 } else {
                     // if variants exist, add all of them from second item to array as first level elements
                     // as variant id, custom attributes
-                    $productsArray[$savedProductId]["variations"]["variants"]["variant"] =  [
-                        "_attributes"
-                        => [
-                            "_product-id" => $data[3],
-                            "default" => $data[6]
-                        ]
+                    $productsArray[$savedProductId]["variations"]["variants"]["variant"][] =  [
+                        "_product-id" => $data[3],
+                        "default" => $defaultVariant
                     ];
 
                     // along with the variants as first level elements, add the id to the main product level as well, along
@@ -106,43 +101,41 @@ class ProductsTest extends DuskTestCase
 
         return $xml;
     }
-    protected function buildProductElement($data): Product
+    protected function buildProductElement($dataItem): Product
     {
-        $element = $this->buildBaseElement('Product', $data);
+        $element = $this->buildBaseElement('Product', $dataItem);
         // $element->setEncoding();
+        // if the product has variations, loop through these
+        if (isset($dataItem['variations'])) {
+            foreach ($dataItem["variations"]["variants"]["variant"] as $variation) {
+                /* $element->setVariants([
+                create an array like this:   
+                        'SKU0000001' => false,
+                        'SKU0000002' => false,
+                        'SKU0000003' => true,
+                    ]);
+                    */
+                $varProdId = $variation["_product-id"];
+                $varDefault = $variation["default"];
+                if (isset($varProdId) && isset($varDefault) && is_string($varProdId) && is_bool($varDefault)) {
+                    $this->variantArray[$varProdId] = $varDefault;
+                }
+            }
+            $element->setVariants($this->variantArray);
+        }
+
 
         return $element;
     }
     protected function buildBaseElement(string $type, $dataItem): Product
     {
-
-        // building the xml 
         // add product id or variant id
         $element = new Product($dataItem["product_id"]);
 
-        //if exists, add display name and brand
+        //if exists, add display name and brand - only exists for main products
         if (isset($dataItem["display_name"])) {
             $element->setDisplayName($dataItem["display_name"]);
             $element->setBrand($dataItem["brand"]);
-            // if the product has variations, loop through these
-            foreach ($dataItem["variations"] as $variation) {
-                /* $element->setVariants([
-                            'SKU0000001' => false,
-                            'SKU0000002' => false,
-                            'SKU0000003' => true,
-                        ]);
-                        */
-                if (
-                    $variation["variant"]["_attributes"]["_product-id"] && $variation["variant"]["_attributes"]["default"]
-                    && is_bool($variation["variant"]["_attributes"]["default"])
-                ) {
-                    $element->setVariants(
-                        [
-                            $variation["variant"]["_attributes"]["_product-id"] => $variation["variant"]["_attributes"]["default"]
-                        ]
-                    );
-                }
-            }
         } else if (count($dataItem["custom-attributes"]) > 0) {
             foreach ($dataItem["custom-attributes"] as $custom_attribute) {
                 $element->addCustomAttributes([
